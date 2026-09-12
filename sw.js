9b1c511d4cd08b3f730864dcc794b081c053d19c
John Altamira <j_altamira@yahoo.com>
2026-09-12 17:00:57 -0400

    Four more engravings; the Rosary is now 12 of 20 illustrated

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cache name carries the build stamp, so every deploy retires the previous cache.
-const BUILD = "20260912-163543";
+const BUILD = "20260912-170057";
 const CACHE = "lamplight-" + BUILD;
 const ASSETS = ["./manifest.webmanifest", "./icon.svg"];
 
