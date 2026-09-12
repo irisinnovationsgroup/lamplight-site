7b164f709eaeafabd6dfae8e820ddb3595a7dfc8
John Altamira <j_altamira@yahoo.com>
2026-09-12 04:16:50 -0400

    Reading for 2026-09-12

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cache name carries the build stamp, so every deploy retires the previous cache.
-const BUILD = "20260911-112002";
+const BUILD = "20260912-041650";
 const CACHE = "lamplight-" + BUILD;
 const ASSETS = ["./manifest.webmanifest", "./icon.svg"];
 
