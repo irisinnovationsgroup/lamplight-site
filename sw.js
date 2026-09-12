9744c761a5c6c9d2a3e557a9dc3d1ac2c70390dd
John Altamira <j_altamira@yahoo.com>
2026-09-12 18:17:36 -0400

    A rosary you can touch, engravings on the appointed readings, 49 prayers, and a prayer you can send

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cache name carries the build stamp, so every deploy retires the previous cache.
-const BUILD = "20260912-174858";
+const BUILD = "20260912-181736";
 const CACHE = "lamplight-" + BUILD;
 const ASSETS = ["./manifest.webmanifest", "./icon.svg"];
 
