c30fd8707e98f7be853ffbd25c97f2186d5db539
John Altamira <j_altamira@yahoo.com>
2026-09-13 01:18:17 -0400

    83 languages of scripture including the Vulgate, the Septuagint and the Leningrad Codex; Italian and nine more interfaces

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cache name carries the build stamp, so every deploy retires the previous cache.
-const BUILD = "20260912-191527";
+const BUILD = "20260913-011817";
 const CACHE = "lamplight-" + BUILD;
 const ASSETS = ["./manifest.webmanifest", "./icon.svg"];
 
